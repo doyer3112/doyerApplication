c65d1b4333afb0f5abb0b315138dae9d8a719ca5
doyer.du <jie.du@qq.com>
2016-11-21 13:18:35 +0800

    location

---
+++ Location/app/src/main/java/doyer/location/LocationService.java
@@ -0,0 +1,173 @@
+package doyer.location;
+
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+
+/**
+ * Created by jie.du on 16/11/17.
+ */
+public class LocationService extends Service {
+
+
+    private int NOTIFICATION = 0;
+
+
+    private void sendNotification(String title, String text) {
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder mBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this).setSmallIcon(R.mipmap.ic_launcher);
+        mBuilder.build().defaults = Notification.DEFAULT_SOUND;
+        mBuilder.setContentText(text);
+        mBuilder.setTicker("Beacon Notification");
+        mBuilder.setContentTitle(title);
+        Uri ringUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        mBuilder.setSound(ringUri);
+        manager.notify(NOTIFICATION++, mBuilder.build());
+    }
+
+    private static final String TAG = "MyLocationService";
+    private LocationManager mLocationManager = null;
+    private static final int LOCATION_INTERVAL = 10 * 1000;
+    private static final float LOCATION_DISTANCE = 10f;
+
+    private class LocationListener implements android.location.LocationListener {
+        Location mLastLocation;
+
+        public LocationListener(String provider) {
+            Log.e(TAG, "LocationListener " + provider);
+            mLastLocation = new Location(provider);
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.e(TAG, "onLocationChanged: " + location);
+            mLastLocation.set(location);
+            showLocation(location);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.e(TAG, "onProviderDisabled: " + provider);
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.e(TAG, "onProviderEnabled: " + provider);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Log.e(TAG, "onStatusChanged: " + provider);
+        }
+    }
+
+    /*
+    LocationListener[] mLocationListeners = new LocationListener[]{
+            new LocationListener(LocationManager.GPS_PROVIDER),
+            new LocationListener(LocationManager.NETWORK_PROVIDER)
+    };
+    */
+
+    LocationListener[] mLocationListeners = new LocationListener[]{
+            new LocationListener(LocationManager.PASSIVE_PROVIDER), new LocationListener(LocationManager.NETWORK_PROVIDER)
+    };
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.e(TAG, "onStartCommand");
+        super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
+    }
+
+    @Override
+    public void onCreate() {
+
+        Log.e(TAG, "onCreate");
+
+        initializeLocationManager();
+
+        try {
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    LOCATION_INTERVAL,
+                    LOCATION_DISTANCE,
+                    mLocationListeners[1]
+            );
+        } catch (java.lang.SecurityException ex) {
+            Log.i(TAG, "fail to request location update, ignore", ex);
+        } catch (IllegalArgumentException ex) {
+            Log.d(TAG, "network provider does not exist, " + ex.getMessage());
+        }
+
+        /*try {
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    LOCATION_INTERVAL,
+                    LOCATION_DISTANCE,
+                    mLocationListeners[1]
+            );
+        } catch (java.lang.SecurityException ex) {
+            Log.i(TAG, "fail to request location update, ignore", ex);
+        } catch (IllegalArgumentException ex) {
+            Log.d(TAG, "gps provider does not exist " + ex.getMessage());
+        }*/
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.e(TAG, "onDestroy");
+        super.onDestroy();
+        if (mLocationManager != null) {
+            for (int i = 0; i < mLocationListeners.length; i++) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    mLocationManager.removeUpdates(mLocationListeners[i]);
+                } catch (Exception ex) {
+                    Log.i(TAG, "fail to remove location listener, ignore", ex);
+                }
+            }
+        }
+    }
+
+    private void initializeLocationManager() {
+        Log.e(TAG, "initializeLocationManager - LOCATION_INTERVAL: " + LOCATION_INTERVAL + " LOCATION_DISTANCE: " + LOCATION_DISTANCE);
+        if (mLocationManager == null) {
+            mLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        }
+    }
+
+    private void showLocation(Location location) {
+        if (location != null) {
+            if (Build.VERSION.SDK_INT >= 23) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+            } else
+                mLocationManager.removeUpdates(mLocationListeners[1]);
+
+            sendNotification("", "纬度：" + location.getLatitude() + "经度： " + location.getLongitude() + "");
+        }
+    }
+}
+
---
+++ Location/app/src/main/java/doyer/location/MainActivity.java
@@ -0,0 +1,274 @@
+package doyer.location;
+
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.cloudnapps.proximity.magic.sync.MainManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_LOCATION = 101;
+    private static final int PERMISSION_LOCATION_TWO = 102;
+    private LocationManager locationManager;
+    private String locationProvider;
+    TextView Lo;
+    TextView La;
+    TextView Imsi;
+    private List<String> providers = new ArrayList<>();
+    private Location gpslocation;
+    private Location networkLocation;
+    private String TAG = "cloudnapps";
+    private int NOTIFICATION = 0;
+    private PendingIntent mLocationPendingIntent;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Lo = (TextView) findViewById(R.id.lo);
+        La = (TextView) findViewById(R.id.la);
+        Imsi = (TextView) findViewById(R.id.imsi);
+
+        locationService();
+        MainManager mainManager = new MainManager(this);
+        mainManager.setup();
+//        initlocation();
+
+        getSimInfo();
+
+
+    }
+
+    private void locationService() {
+        Intent intent = new Intent(
+                this, LocationService.class);
+        mLocationPendingIntent =
+                PendingIntent.getService(this, 0, intent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+        startService(intent);
+    }
+
+    private void getSimInfo() {
+        TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String imsi = tm.getSubscriberId();
+        Imsi.setText("getSimInfo Imsi: " + tm.toString());
+        Log.d(TAG, "getSimInfo Imsi: " + tm.getSubscriberId() + "  " + tm.getDeviceId() + "  " + tm.getSimOperator() + "  " + tm.getSimOperatorName() + "   " + tm.getSimSerialNumber());
+    }
+
+    private void initlocation() {
+
+        //获取地理位置管理器
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_LOCATION_TWO);
+            }
+
+        }
+        //获取所有可用的位置提供器
+        providers = locationManager.getProviders(true);
+        //获取Location
+
+        setProvider();
+
+//        getLocation();
+
+//        locationProvider = getProvider();
+        Location location = locationManager.getLastKnownLocation(locationProvider);
+        //监视地理位置变化
+//        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        locationManager.requestLocationUpdates(locationProvider, 10 * 1000, 300, mLocationPendingIntent);
+
+    }
+
+    private void setProvider() {
+        /*if (providers.contains(LocationManager.NETWORK_PROVIDER)) {
+            //如果是Network
+            locationProvider = LocationManager.NETWORK_PROVIDER;
+        } else if (providers.contains(LocationManager.GPS_PROVIDER)) {
+            //如果是GPS
+            locationProvider = LocationManager.GPS_PROVIDER;
+        } else */
+        if (providers.contains(LocationManager.PASSIVE_PROVIDER)) {
+            locationProvider = LocationManager.PASSIVE_PROVIDER;
+        } else {
+            Toast.makeText(this, "没有可用的位置提供器", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+    private Location getLocation() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_LOCATION_TWO);
+            }
+            return locationOperation();
+        } else {
+            return locationOperation();
+        }
+    }
+
+    private Location locationOperation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_LOCATION);
+        }
+        try {
+            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, locationListener);
+                gpslocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            }
+            if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 1, locationListener);
+                networkLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            }
+        } catch (IllegalArgumentException e) {
+            Log.e("error", e.toString());
+        }
+        if (gpslocation == null && networkLocation == null)
+            return null;
+
+        if (gpslocation != null && networkLocation != null) {
+            if (gpslocation.getTime() < networkLocation.getTime()) {
+                gpslocation = null;
+                return networkLocation;
+            } else {
+                networkLocation = null;
+                return gpslocation;
+            }
+        }
+        if (gpslocation == null) {
+            return networkLocation;
+        }
+        return null;
+    }
+
+    LocationListener locationListener = new LocationListener() {
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle arg2) {
+            Log.d(TAG, "onStatusChanged: ");
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.d(TAG, "onProviderEnabled: ");
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.d(TAG, "onProviderDisabled: ");
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.d(TAG, "onLocationChanged: ");
+            //如果位置发生变化,重新显示
+            showLocation(location);
+
+        }
+    };
+
+    private void showLocation(Location location) {
+        if (location != null) {
+            if (Build.VERSION.SDK_INT >= 23) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    requestLocationPermission();
+                    return;
+                }
+            } else
+                locationManager.removeUpdates(locationListener);
+
+            La.setText(location.getLatitude() + " 纬度");
+            Lo.setText(location.getLongitude() + " 经度");
+            sendNotification("", "纬度：" + location.getLatitude() + "经度： " + location.getLongitude() + "");
+        }
+    }
+
+    private void requestLocationPermission() {
+        // 摄像头权限已经被拒绝
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // 如果用户已经拒绝劝降，那么提供额外的权限说明
+        }
+    }
+
+    // 获取Location Provider
+    private String getProvider() {
+        // 构建位置查询条件
+        Criteria criteria = new Criteria();
+        // 查询精度：高
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        // 是否查询海拨：否
+        criteria.setAltitudeRequired(false);
+        // 是否查询方位角 : 否
+        criteria.setBearingRequired(false);
+        // 是否允许付费：是
+        criteria.setCostAllowed(true);
+        // 电量要求：低
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        // 返回最合适的符合条件的provider，第2个参数为true说明 , 如果只有一个provider是有效的,则返回当前provider
+        return locationManager.getBestProvider(criteria, true);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_LOCATION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //获取所有可用的位置提供器
+                providers = locationManager.getProviders(true);
+            } else {
+                Toast.makeText(MainActivity.this, permissions[0] + "被拒", Toast.LENGTH_LONG).show();
+            }
+        } else if (requestCode == PERMISSION_LOCATION_TWO) {
+
+        }
+
+    }
+
+
+    private void sendNotification(String title, String text) {
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder mBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(MainActivity.this).setSmallIcon(R.mipmap.ic_launcher);
+        mBuilder.build().defaults = Notification.DEFAULT_SOUND;
+        mBuilder.setContentText(text);
+        mBuilder.setTicker("Beacon Notification");
+        mBuilder.setContentTitle(title);
+        Uri ringUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        mBuilder.setSound(ringUri);
+        manager.notify(NOTIFICATION++, mBuilder.build());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        locationManager.removeUpdates(mLocationPendingIntent);
+    }
+}
